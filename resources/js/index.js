32e50c0fc0d811e74504d2de2a3e4ade3414490c
beadle <15750757270@163.com>
2015-07-15 16:02:58 +0800

    update the new blog

---
+++ resources/js/index.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	var categorySignHtml = "<span class=\"glyphicon glyphicon-play\" aria-hidden=\"true\"></span>";
+	$('.right_panel .panel-success .panel-body ul li').prepend(categorySignHtml);
+});
