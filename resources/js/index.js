6ca58a048f648ef42a018f10b24a07eabe5a5272
beadle <15750757270@163.com>
2015-07-16 11:33:56 +0800

    update

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,36 +3,31 @@ $(document).ready(function(){
 	$.ajax({
 		url:"resources/json/index.json",
 		type:"GET",
-		dataType : 'text',
+		dataType : 'json',
 		success:function(data){
-			var str = data.replace(new RegExp("\r\n","gm"),"");
-			alert(str);
-			var da = eval('('+str+')');
 			//只取前6个
-//			for(var i=0;i < da.length && i < 6;i++){
-//				var indexBlogHTML = "<a href='"+da[i].url+"' class='list-group-item'>"+
-//										"<h4 class='list-group-item-heading'>"+
-//											"<div class='head_title'>"+da[i].title+"</div>"+
-//											"<div class='head_date'>日期:"+da[i].date+"</div>"+
-//										"</h4>"+
-//										"<p class='list-group-item-text'>"+da[i].description+"</p>"+
-//									"</a>";
-//				$('.body_content .list-group').append(indexBlogHTML);
-//			}
+			for(var i=0;i < data.length && i < 6;i++){
+				var indexBlogHTML = "<a href='"+data[i].url+"' class='list-group-item'>"+
+										"<h4 class='list-group-item-heading'>"+
+											"<div class='head_title'>"+data[i].title+"</div>"+
+											"<div class='head_date'>日期:"+data[i].date+"</div>"+
+										"</h4>"+
+										"<p class='list-group-item-text'>"+data[i].description+"</p>"+
+									"</a>";
+				$('.body_content .list-group').append(indexBlogHTML);
+			}
 		}
 	});
 	//get data from category.json fill the index category
 	$.ajax({
 		url:"resources/json/category.json",
 		type:"GET",
-		dataType : 'text',
+		dataType : 'json',
 		success:function(data){
-			var str = data.replace(/\r\n/g, "");
-			var da = eval('('+str+')');
-			for (var key in da)
+			for (var key in data)
 		    {
 		        var $category = $('.body_content .right_panel .panel-success .panel-body ul');
-		        var categoryLiHTML = "<li><a href=content/category.html#category="+key+">"+da[key].title+"</a></li>"
+		        var categoryLiHTML = "<li><a href=content/category.html#category="+key+">"+data[key].title+"</a></li>"
 		        $category.append(categoryLiHTML);
 		    }
 			//category li decorate
