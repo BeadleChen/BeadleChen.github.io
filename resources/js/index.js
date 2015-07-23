fde2abebd674727385da3bab77b557a52a343da7
Beadle <hellobeadle@163.com>
2015-07-23 15:50:34 +0800

    update

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,15 +1,13 @@
-$(document).ready(function(){
-	//get data from category.json fill the index category
+//get data from category.json fill the index category
 
-	ajax("resources/json/category.json","GET",function(data){
-		data = eval('('+data+')');
-		for (var key in data){
-		        var liNode = document.createElement("li");
-				var aNode = document.createElement("a");
-				aNode.href = "content/category.html#category="+key;
-				aNode.textContent = data[key].title;
-		        liNode.appendChild(aNode)
-				document.getElementById("category_id").appendChild(liNode);		 
-		}
-	});
+ajax("resources/json/category.json","GET",function(data){
+	data = eval('('+data+')');
+	for (var key in data){
+			var liNode = document.createElement("li");
+			var aNode = document.createElement("a");
+			aNode.href = "content/category.html#category="+key;
+			aNode.textContent = data[key].title;
+			liNode.appendChild(aNode)
+			document.getElementById("category_id").appendChild(liNode);		 
+	}
 });
