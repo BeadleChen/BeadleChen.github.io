f1bc14dedf5f2f7d040c261b8e66e38d3f4a71d5
Beadle <hellobeadle@163.com>
2015-07-27 18:22:34 +0800

    update

--- resources/js/category.js
+++ resources/js/category.js
@@ -9,19 +9,47 @@ if(checkValueIsEmpty(category)){
 ajax("../resources/json/category.json","GET",function(data){
 	data = eval('('+data+')');
 	var container = eval('data.'+category);
+	//fill to content title
 	document.getElementById('content_title').textContent=container.title;
 	var content = container.content;
-	for(var i=0;i<content.length;i++){
-		var title = content[i].title;
-		var article = content[i].article;
-		var url = 'blog.html#category='+category+'#article='+article;
-		var liNode = document.createElement("li");
-		var aNode = document.createElement("a");
-		aNode.href = url;
-		aNode.textContent = title;
-		liNode.appendChild(aNode);
-		document.getElementById('content_ul').appendChild(liNode);
+
+	var categoryContent = document.getElementById('category_content');
+	//whether it is message 
+	if( category == 'message'){
+		ajax("../resources/json/message.json","GET",function(data){
+			data = eval('('+data+')');
+			for(var i=0;i<data.length;i++){
+				var messageFrame = document.createElement("div");
+				var messageName = document.createElement("div");
+				var messageContent = document.createElement("div");
+				messageContent.id = 'message_content';
+				messageName.id = 'message_name';
+				messageFrame.id = 'message_frame';
+				messageName.textContent = data[i].name+'说:';
+				messageContent.textContent = data[i].content;
+				messageFrame.appendChild(messageName);
+				messageFrame.appendChild(messageContent);
+				categoryContent.appendChild(messageFrame);
+			}
+			
+		});
+	}else{
+		var contentUL = document.createElement("ul");
+		contentUL.id = 'content_ul';
+		categoryContent.appendChild(contentUL);
+		for(var i=0;i<content.length;i++){
+			var title = content[i].title;
+			var article = content[i].article;
+			var url = 'blog.html#category='+category+'#article='+article;
+			var liNode = document.createElement("li");
+			var aNode = document.createElement("a");
+			aNode.href = url;
+			aNode.textContent = title;
+			liNode.appendChild(aNode);
+			document.getElementById('content_ul').appendChild(liNode);
+		}
 	}
+	
 	//fill the index
 	document.getElementById('current_category').textContent=category;
 });
